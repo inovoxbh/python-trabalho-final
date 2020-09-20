d2747f23683286ddf57ba2db5c18fc3f922f27cf
Bruno G Alcantara <inovox@gmail.com>
2020-09-20 00:08:50 -0300

    fluxo previsto

--- fluxocaixa/views/vfluxo.py
+++ fluxocaixa/views/vfluxo.py
@@ -39,70 +39,94 @@ def previsto(request):
     db_saldo_inicial = SaldoInicial.objects.all().order_by('id')
     for i in db_saldo_inicial:
         valorinicial =i.valorinicial
-    print(valorinicial)
     
     valorreceitastotal =0
-    valorreceitasmes =0
-
     valordespesastotal =0
-    valordespesasmes =0
 
     dados ="<div>"
 
     if (db_lista_pagar or db_lista_receber):
-        anoant =0
-        mesant =0
-
-        # despesas
-        if (db_lista_pagar):
-            dados += "<h1>Despesas Previstas</h1>"
-            for i in db_lista_pagar:
-                titulo = TituloPagar.objects.get(id=i.id)
-                
-                ano =titulo.data_vencimento.year
-                mes =titulo.data_vencimento.month
-
-                valordespesastotal += titulo.valor
-
-                if ((ano != anoant) or (mes != mesant)):
-                    if ((anoant >0) and (mesant >0)):
-                        dados += "</table>"
-
-                    dados += "<h2>" + nomedomes(mes) + " de " + str(ano) + "</h2>"
-                    dados += "<table border='1'>"
-                    dados += "<tr style='background-color: orange'><th>Classificação</th><th>Descrição</th><th>Valor</th><th>Vencimento</th></tr>"
-                    anoant =ano
-                    mesant =mes
-                
-                dados += "<tr><td>" + str(titulo.classificacao) + "</td><td>" + str(titulo.descricao) + "</td><td>" + str(titulo.valor) + "</td><td>" + str(titulo.data_vencimento) + "</tr>"
-            dados += "</table>"
-
         # receitas
         if (db_lista_receber):
-            dados += "<h1>Receitas Previstas</h1>"
+            anoant =0
+            mesant =0
+
+            dados += "<h1 style='background-color: green'>Receitas Previstas</h1>"
             for i in db_lista_receber:
                 titulo = TituloReceber.objects.get(id=i.id)
                 
                 ano =titulo.data_expectativa.year
                 mes =titulo.data_expectativa.month
 
-                valorreceitastotal += titulo.valor
-
                 if ((ano != anoant) or (mes != mesant)):
                     if ((anoant >0) and (mesant >0)):
                         dados += "</table>"
+                        dados += "<h3>Final: R$ " + str(valorreceitastotal) + "</h3>"
+                        dados += "<br />"
 
                     dados += "<h2>" + nomedomes(mes) + " de " + str(ano) + "</h2>"
+                    if ((anoant ==0) and (mesant ==0)):
+                        dados += "<h3>Inicial: R$ " + str(valorinicial) + "</h3>"
+                        valorreceitastotal =valorinicial
+                    else:
+                        dados += "<h3>Inicial: R$ " + str(valorreceitastotal) + "</h3>"
                     dados += "<table border='1'>"
                     dados += "<tr style='background-color: green'><th>Classificação</th><th>Descrição</th><th>Valor</th><th>Expectativa</th></tr>"
                     anoant =ano
                     mesant =mes
                 
+                valorreceitastotal += titulo.valor
+
                 dados += "<tr><td>" + str(titulo.classificacao) + "</td><td>" + str(titulo.descricao) + "</td><td>" + str(titulo.valor) + "</td><td>" + str(titulo.data_expectativa) + "</tr>"
             dados += "</table>"
+            dados += "<h3>Final: R$ " + str(valorreceitastotal) + "</h3>"
+            dados += "<br />"
 
-        dados += "<h3>Total despesas: R$ " + str(valordespesastotal) + "</h3>"
-        dados += "<h3>Total receitas: R$ " + str(valorreceitastotal) + "</h3>"
+        # despesas
+        if (db_lista_pagar):
+            anoant =0
+            mesant =0
+
+            dados += "<h1 style='background-color: orange'>Despesas Previstas</h1>"
+            for i in db_lista_pagar:
+                titulo = TituloPagar.objects.get(id=i.id)
+                
+                ano =titulo.data_vencimento.year
+                mes =titulo.data_vencimento.month
+
+                if ((ano != anoant) or (mes != mesant)):
+                    if ((anoant >0) and (mesant >0)):
+                        dados += "</table>"
+                        dados += "<h3>Final: R$ " + str(valordespesastotal) + "</h3>"
+                        dados += "<br />"
+
+                    dados += "<h2>" + nomedomes(mes) + " de " + str(ano) + "</h2>"
+                    if ((anoant ==0) and (mesant ==0)):
+                        dados += "<h3>Inicial: R$ " + str(valorinicial) + "</h3>"
+                        valordespesastotal =valorinicial
+                    else:
+                        dados += "<h3>Inicial: R$ " + str(valordespesastotal) + "</h3>"
+                    dados += "<table border='1'>"
+                    dados += "<tr style='background-color: orange'><th>Classificação</th><th>Descrição</th><th>Valor</th><th>Vencimento</th></tr>"
+                    anoant =ano
+                    mesant =mes
+                
+                valordespesastotal += titulo.valor
+
+                dados += "<tr><td>" + str(titulo.classificacao) + "</td><td>" + str(titulo.descricao) + "</td><td>" + str(titulo.valor) + "</td><td>" + str(titulo.data_vencimento) + "</tr>"
+            dados += "</table>"
+            dados += "<h3>Final: R$ " + str(valordespesastotal) + "</h3>"
+            dados += "<br />"
+
+        dados += "<br />"
+        dados += "<br />"
+        dados += "<h1 style='background-color: gray'>Resultado</h1>"
+        dados += "<h2>Total receitas: R$ " + str(valorreceitastotal) + "</h2>"
+        dados += "<h2>Total despesas: R$ " + str(valordespesastotal) + "</h2>"
+        saldofinal =valorreceitastotal - valordespesastotal
+        dados += "<h2>Saldo final: R$ " + str(saldofinal) + "</h2>"
+        lucratividade =saldofinal - valorinicial
+        dados += "<h2>Lucratividade: R$ " + str(lucratividade) + "</h2>"
     else:
         dados += "<p>Não foram localizados registros para exibição.</p>"
 
